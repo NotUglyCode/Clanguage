a9f02955f58ada3b13134fd9d76a8222fd1e7ab3
NotUglyCode <47150057+NotUglyCode@users.noreply.github.com>
2019-03-04 01:02:57 +0900

    Create t.c

---
+++ t.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main(void)
+{
+	int xpos;
+	
+	return 0;
+}
+
+if ( size > 12 ){
+	cost = cost * 1.05;
+	flag = 2;
+}
+bill = cost * flag;
+
+* 둘 중에서 하나를 선택하는 경우
+
+if ( ibex > 14 ){
+	goto a;
+	sheds = 2;
+	goto b;
+	a: sheds = 3;
+	b: help 2 * sheds;
+} 
+
+if ( ibex > 14 )
+{
+	sheds = 3;
+}
+else
+{
+	sheds = 2;
+	help = 2 * sheds;
+}
+
+readin: scanf("%d",&score);
+if ( score < 0 )
+	goto stage2;
+	goto readin;
+	stage2: more up take IT
+
+
